0ede8d72a8377d439f6bea8281f5bd79cafff936
Nick Remijn <nick.remijn@gmail.com>
2015-12-18 14:36:58 +0100

    added functionality to switch to a new namespace Added warning boxes on connection issues/messages

--- public/websocket.js
+++ public/websocket.js
@@ -3,21 +3,35 @@ var WebSocket = function(endpoint){
     console.log("connecting to " + endpoint);
     this.socket = io(endpoint);
     self.onStatusChange("connecting");
-    this.socket.on('data', function (data) {
-        self.onData(data);
-    });
-    this.socket.on("connect", function(){
-        self.onStatusChange("connected");
-    });
-    this.socket.on("connect_timeout", function(){
-        self.onStatusChange("timeout");
-    });
-    this.socket.on("reconnecting", function(){
-        self.onStatusChange("reconnecting");
-    });
-    this.socket.on("reconnect", function(){
-        self.onStatusChange("reconnected");
-    })
+Q    self.updateEvents = function(){
+        self.socket.on('data', function (data) {
+            self.onData(data);
+        });
+        self.socket.on("connect", function(){
+            self.onStatusChange("connected");
+        });
+        self.socket.on("connect_timeout", function(){
+            self.onStatusChange("timeout");
+        });
+        self.socket.on("reconnecting", function(){
+            self.onStatusChange("reconnecting");
+        });
+        self.socket.on("reconnect", function(){
+            self.onStatusChange("reconnected");
+        });
+    };
+    this.updateEvents();
+};
+
+WebSocket.prototype.connect = function(endpoint){
+    this.socket.disconnect();
+    delete this.socket;
+
+    console.log("connecting to " + endpoint);
+    this.socket = io(endpoint);
+    this.onStatusChange("connecting");
+
+    this.updateEvents();
 };
 
 WebSocket.prototype.onData = function(data){
