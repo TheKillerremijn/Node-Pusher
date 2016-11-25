0e00a3a5d24c672b66cddf1ab6f08309234a542b
Nick Remijn <nick.remijn@gmail.com>
2016-11-25 17:09:06 +0100

    test

--- Graphlr/PushRoutes.js
+++ Graphlr/PushRoutes.js
@@ -6,14 +6,26 @@ exports.match = function(route, tomatch){
     var tomatchsplit = tomatch.split(':');
 
     for(var i=0;i<tomatchsplit.length;i++){
-        if(tomatchsplit[i] == "*" || routesplit[i] == "*") continue;
+        if(tomatchsplit[i] == "*" || routesplit[i] == "*"){
+
+            if(routesplit.length <= i){
+                return true;
+            }
+
+            continue;
+        }
         if(tomatchsplit[i] == routesplit[i]){
             continue;
         }else{
             return false;
         }
     }
+    // prepr:123456:*
+    // prepr:*     :test:abc
+
+
     return true;
+
 };
 exports.matchDirectional = function(route, tomatch){
 
